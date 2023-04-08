16ee7350a02d6440f273ab917268c17211396dd7
Kol Influence <104323920+kolinfluence@users.noreply.github.com>
2023-04-08 07:55:02 -0700

    Create solrandom.sol

---
+++ solrandom.sol
@@ -0,0 +1,12 @@
+
+function randomUint8(uint8 number) internal view returns (uint8) {
+    return uint8(uint8(blockhash(block.number - 1)) % number);
+}
+
+function randomUint16(uint8 number) internal view returns (uint16) {
+    return uint16(uint16(blockhash(block.number - 1)) % number);
+}
+
+function randomUint32(uint8 number) internal view returns (uint32) {
+    return uint32(uint32(blockhash(block.number - 1)) % number);
+}
